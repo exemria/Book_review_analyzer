1ae627ca8e7cc368a9ff609b0f6bff499e572738
Martyna <mbuczek.dev@gmail.com>
2023-12-03 22:54:40 +0100

    New changes

--- setup_tables.py
+++ setup_tables.py
@@ -34,95 +34,114 @@ class Db_books_review:
     
     def commit(self):
         self.conn.commit()
+    
+    def execute(self):
+        self.cur.execute()
 
       
 # download all tables from database amazon if exists
-def get_table_list(self):
-    self.cur.execute("""
-        SELECT books, reviews
-        FROM information_schema.tables
-        WHERE table_schema = 'public'
-    """)
-    return self.cur.fetchall()
+    def get_table_list(self):
+        self.cur.execute("""
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+        """)
+        return self.cur.fetchall()
 
-def drop_all_tables(self,tables):
-    for table in tables:
-        self.cur.execute(f" DROP TABLE IF EXISTS {table[0]} CASCADE")
-    self.conn.commit()
+    def drop_all_tables(self,tables):
+        for table in tables:
+            self.cur.execute(f" DROP TABLE IF EXISTS {table[0]} CASCADE")
+        self.conn.commit()
 
-def create_table_books(self):
-    self.cur.execute('''
-        CREATE TABLE "books" (
-        "rank" FLOAT,
-        "book_title" VARCHAR(255) PRIMARY KEY,
-        "book_price" VARCHAR(255),
-        "rating" FLOAT,
-        "author" VARCHAR(255),
-        "year_of_publication" TEXT,
-        "genre" VARCHAR(255),
-        "url" VARCHAR(255)
-    )
-    ''')
-    self.conn.commit()
-
-def create_table_reviews(self):
-    self.cur.execute('''
-        CREATE TABLE "reviews" (
-            "sno" FLOAT PRIMARY KEY,
-            "book_name" VARCHAR(255),
-            "review_title" VARCHAR(255),
-            "reviewer" VARCHAR(255),
+    def create_table_books(self):
+        self.cur.execute('''
+            CREATE TABLE "books" (
+            "rank" FLOAT,
+            "book_title" VARCHAR(255) PRIMARY KEY,
+            "book_price" VARCHAR(255),
             "rating" FLOAT,
-            "review_description" TEXT,
-            "js_verified" VARCHAR(255), 
-            "date" NUMERIC, 
-            "timestamp" VARCHAR(255), 
-            "asin" VARCHAR(255),
-        FOREIGN KEY ("book_name") REFERENCES "books"("book_title"))
-    ''')
-    self.conn.commit()
-    
+            "author" VARCHAR(255),
+            "year_of_publication" TEXT,
+            "genre" VARCHAR(255),
+            "url" VARCHAR(255)
+        )
+        ''')
+        self.conn.commit()
 
-def insert_data_from_csv(self, file_path1,   file_path2):
-    books_data = {}
-    with open(file_path1, 'r', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader)
-        for row in reader:
-            row = [None if cell == '' else cell for cell in row]
-            book_title = row[1]
-            books_data[book_title] = row
-            
-            self.cur.execute("""INSERT INTO reviews (Sno, book_name, review_title, reviewer, rating, review_description, js_verified, date, timestamp, ASIN)
-                                VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", row)
-            self.conn.commit()    
-            
+    def create_table_reviews(self):
+        self.cur.execute('''
+            CREATE TABLE "reviews" (
+                "sno" FLOAT PRIMARY KEY,
+                "book_name" VARCHAR(255),
+                "review_title" VARCHAR(255),
+                "reviewer" VARCHAR(255),
+                "rating" FLOAT,
+                "review_description" TEXT,
+                "js_verified" VARCHAR(255), 
+                "date" NUMERIC, 
+                "timestamp" VARCHAR(255), 
+                "asin" VARCHAR(255),
+            FOREIGN KEY ("book_name") REFERENCES "books"("book_title"))
+        ''')
+        self.conn.commit()
 
-    with open(file_path2, 'r', encoding='utf-8') as reviews_csv:
-        reviews_reader = csv.reader(reviews_csv)
-        next(reviews_reader)
-        for review_row in reviews_reader:
-            # if book_name exists in the books_data, proceed with insertion into reviews table
-            book_name = review_row[1] 
-            if book_name in books_data:
-                # Convert date string to a numeric representation (timestamp)
-                date_str = review_row[7]  # Assuming date is at index 7
-                try:
-                    date_numeric = datetime.strptime(date_str, '%d-%m-%Y').timestamp()
-                except ValueError:
-                    date_numeric = None
-                review_row[7] = date_numeric
-                self.cur.execute("""
-                    INSERT INTO reviews (Sno, book_name, review_title, reviewer, rating,
-                                        review_description, js_verified, date, timestamp, ASIN)
-                    VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-                """, review_row)
-                self.conn.commit()
+
+    def insert_data_from_csv(self, file_path1, file_path2):
+        try:
+
+            books_data = {}
+            with open(file_path1, 'r', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile)
+                next(reader)
+                for row in reader:
+                    row = [None if cell == '' else cell for cell in row]
+                    self.cur.execute("""
+                        INSERT INTO books (rank, book_title, book_price, rating, author, year_of_publication, genre, url)
+                        VALUES(%s,%s,%s,%s,%s,%s,%s,%s)
+                    """,row)
+                    self.conn.commit() 
+                    book_title = row[1]
+                    books_data[book_title] = row
+            
+                    #print(books_data)
+        except psycopg2.IntegrityError as e:
+            if 'duplicate key value violates unique constraint "books_pkey"' in str(e):
+                print("Duplicate book_title found. Handling duplicate...")
+                # Handle the duplicate entry: update existing record or skip insertion
             else:
-                print(f"Book '{book_name}' does not exist in the 'books' table. Skipping insertion into 'reviews'.")
-        
-                    
+                print("Error inserting data into the books table:", e)
+
+        book_name = []
+        with open(file_path2, 'r', encoding='utf-8') as reviews_csv:
+            reviews_reader = csv.reader(reviews_csv)
+            next(reviews_reader)
             
+            for review_row in reviews_reader:
+                # if book_name exists in the books_data, proceed with insertion into reviews table
+                review_book = review_row[1]
+                book_name.append(review_book)
+               
+
+                for book in book_name:
+
+                    if book in books_data.values():
+                        # Convert date string to a numeric representation (timestamp)
+                        date_str = review_row[7]  # Assuming date is at index 7
+                        try:
+                            date_numeric = datetime.strptime(date_str, '%d-%m-%Y').timestamp()
+                        except ValueError:
+                            date_numeric = None
+                        review_row[7] = date_numeric
+                        self.cur.execute("""
+                            INSERT INTO reviews (Sno, book_name, review_title, reviewer, rating,
+                                                review_description, js_verified, date, timestamp, ASIN)
+                            VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                        """, review_row)
+                        self.conn.commit()
+                        #print(f"Book {book_name} added.")
+                    else:
+                        None #print(f"Book {book_name} does not exist in the books_data. Skipping insertion into 'reviews'.")
+            #print(f" {book_name}")
 
 def main():
     
@@ -150,7 +169,7 @@ def main():
         return
     
     try:
-        
+        db_action.drop_all_tables(tables)
         db_action.create_table_books()
         db_action.create_table_reviews()
         
@@ -160,10 +179,10 @@ def main():
 
     try:
         
-        relative_path_to_bookscsv = "resources\Top_100_Trending_Books.csv"
-        relative_path_to_reiewscsv = "resources\customer_reviews.csv"
-        csv_file_books_path = return_absolute_path(relative_path_to_bookscsv)
-        csv_file_reviews_path = return_absolute_path(relative_path_to_reviewscsv)
+        #relative_path_to_bookscsv = "resources\Top_100_Trending_Books.csv"
+       # relative_path_to_reviewscsv = r"resources\customer_reviews.csv"
+        csv_file_books_path = return_absolute_path( "resources\Top_100_Trending_Books.csv")
+        csv_file_reviews_path = return_absolute_path('resources\customer_reviews.csv')
         
         db_action.insert_data_from_csv(csv_file_books_path, csv_file_reviews_path)
 
